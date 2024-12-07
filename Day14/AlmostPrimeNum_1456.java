f441e45fb118c89e585f49b151dbbfd02e4213aa
notebook <wnsdh8846@naver.com>
2024-12-07 22:20:20 +0900

    day14

--- Day14/AlmostPrimeNum_1456.java
+++ Day14/AlmostPrimeNum_1456.java
@@ -1,39 +1,56 @@
 package Day14;
-
 import java.util.Scanner;
 
-public class AlmostPrimeNum_1456 {
+public class AlmostPrimeNum_1456{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		long MM = sc.nextInt();
-		long NN = sc.nextInt();
+		long MM = sc.nextLong();
+		long NN = sc.nextLong();
 		int M = (int)Math.sqrt(MM);
 		int N = (int)Math.sqrt(NN);
 		
-		int arr[] = new int[(int)(N+1)];
+		int arr[];
 		
-		for(int i=M; i<=N; i++) {
+		arr = new int[(int)(N+1)];
+		
+		
+		
+		for(int i=0; i<=N; i++) {
+			if(i==0||i==1) {
+				arr[i]=-1;
+			}
 			arr[i]=i;
 		}
-		int count = 0;
+		
 		for(int i=2; i<=N; i++) {
-			int sum = i;
+			long sum = i;
 			if(arr[i]==i) {
 				for(int j=2; ; j++) {
 					sum= i*j;
-					if(sum>Math.sqrt(N)) {
+					if(sum>Math.sqrt(NN)) {
 						break;
 					}
-					arr[sum]=-1;
-				}
-				if(i*i<=NN && i*i>=MM) {
-					System.out.print(i*i+" ");
-					count++;
+					arr[(int)sum]=-1;
 				}
 			}
-			
+		}
+
+		int count = 0;
+		
+		for(int i=2; i<=N; i++) {
+			if(arr[i]!=-1) {
+				long sum = i;
+				do {
+					sum*=i;
+					if(sum<=NN && sum>=MM) {
+						count++;
+//						System.out.print(sum+" ");
+					}
+				}
+				while(sum<=NN);
+			}
 		}
 		
 		System.out.println(count);
